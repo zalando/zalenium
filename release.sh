867406ee464398624891a36b6eaff05c9673a052
Diego Fernando Molina Bocanegra <diego.fernando.molina.bocanegra@zalando.de>
2016-10-07 18:20:09 +0200

    [BUA-619] Setting release process

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+DEFAULT_VERSION=1.0-SNAPSHOT
+RELEASE_VERSION=${1?"Usage $0 release-version [next-version]"}
+NEXT_VERSION=${2-${DEFAULT_VERSION}}
+
+# release
+mvn scm:check-local-modification versions:set -DnewVersion=${RELEASE_VERSION} scm:add -Dincludes="**/pom.xml" scm:checkin -Dmessage="Release $RELEASE_VERSION"
+
+mvn -Prelease clean scm:tag deploy
+
+# next development version
+mvn versions:set -DnewVersion=${NEXT_VERSION} scm:add -Dincludes="**/pom.xml" scm:checkin -Dmessage="Develop $NEXT_VERSION"
