84b0bf6ffa68b6f8afe233d762acab3b7f84f7a9
Diego Molina <diemol@users.noreply.github.com>
2016-10-09 12:29:00 +0200

    Setting up release process. (#1)
    * [BUA-619] Setting release process
    
    * [BUA-619] Grabbing ZALENIUM_ARTIFACT from the maven final artefact name.
    
    * [BUA-619] Putting a prefix before the version in the zipped file name.
    
    * [BUA-619] Adding one prerequisite.
    
    * [BUA-619] All tests were doing the same thing, 2 is more than enough.
    
    * [BUA-619] Changing back the scm values.

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+RELEASE_VERSION=${1?"Usage $0 release-version [next-version]"}
+NEXT_VERSION=${2?"Usage $0 release-version [next-version]. Parameter 'next-version' is missing."}
+
+# release
+mvn scm:check-local-modification versions:set -DnewVersion=${RELEASE_VERSION} scm:add -Dincludes="**/pom.xml" scm:checkin -Dmessage="Release $RELEASE_VERSION"
+
+mvn -Prelease clean scm:tag package
+
+# next development version
+mvn versions:set -DnewVersion=${NEXT_VERSION} scm:add -Dincludes="**/pom.xml" scm:checkin -Dmessage="Develop $NEXT_VERSION"
