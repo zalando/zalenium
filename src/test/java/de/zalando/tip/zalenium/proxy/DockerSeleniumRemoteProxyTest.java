929fbd92fbe2b4e2105af106cc27a4a53f8b4f1f
Diego Molina <diemol@users.noreply.github.com>
2016-10-20 18:07:13 +0200

    BUA 689 (#3)
    * [BUA-689] Removing unnecesary data types.
    
    * [BUA-689] Adding unit tests for DSProxy

---
+++ src/test/java/de/zalando/tip/zalenium/proxy/DockerSeleniumRemoteProxyTest.java
@@ -0,0 +1,61 @@
+package de.zalando.tip.zalenium.proxy;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.grid.common.GridRole;
+import org.openqa.grid.common.RegistrationRequest;
+import org.openqa.grid.internal.Registry;
+import org.openqa.grid.internal.TestSession;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.CapabilityType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DockerSeleniumRemoteProxyTest {
+
+    private DockerSeleniumRemoteProxy proxy;
+
+    @Before
+    public void setup() {
+        Registry registry = Registry.newInstance();
+
+        // Creating the configuration and the registration request of the proxy (node)
+        RegistrationRequest request = new RegistrationRequest();
+        request.setRole(GridRole.NODE);
+        request.getConfiguration().put(RegistrationRequest.MAX_SESSION, 5);
+        request.getConfiguration().put(RegistrationRequest.AUTO_REGISTER, true);
+        request.getConfiguration().put(RegistrationRequest.REGISTER_CYCLE, 5000);
+        request.getConfiguration().put(RegistrationRequest.HUB_HOST, "localhost");
+        request.getConfiguration().put(RegistrationRequest.HUB_PORT, 4444);
+        request.getConfiguration().put(RegistrationRequest.PORT, 40000);
+        request.getConfiguration().put(RegistrationRequest.PROXY_CLASS, "de.zalando.tip.zalenium.proxy.DockerSeleniumRemoteProxy");
+        request.getConfiguration().put(RegistrationRequest.REMOTE_HOST, "http://localhost:4444");
+        request.getCapabilities().clear();
+        request.getCapabilities().addAll(DockerSeleniumStarterRemoteProxy.getDockerSeleniumFallbackCapabilities());
+
+        // Creating the proxy
+        proxy = DockerSeleniumRemoteProxy.getNewInstance(request, registry);
+    }
+
+    @Test
+    public void dockerSeleniumOnlyRunsOneTestPerContainer() {
+        // Supported desired capability for the test session
+        Map<String, Object> requestedCapability = new HashMap<>();
+        requestedCapability.put(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
+        requestedCapability.put(CapabilityType.PLATFORM, Platform.LINUX);
+
+        // Not tests have been executed.
+        Assert.assertEquals(0, proxy.getAmountOfExecutedTests());
+
+        TestSession newSession = proxy.getNewSession(requestedCapability);
+
+        Assert.assertNotNull(newSession);
+
+        // One test is/has been executed.
+        Assert.assertEquals(1, proxy.getAmountOfExecutedTests());
+    }
+
+}
