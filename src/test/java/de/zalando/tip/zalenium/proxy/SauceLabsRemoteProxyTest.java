638b86d39e5261e04bb7fca1253fbef9abc0cd9f
Diego Fernando Molina Bocanegra <diego.fernando.molina.bocanegra@zalando.de>
2016-10-30 19:15:37 +0100

    [BUA-716] Adding unit test for SauceLabs proxy beforeMethod.

--- src/test/java/de/zalando/tip/zalenium/proxy/SauceLabsRemoteProxyTest.java
+++ src/test/java/de/zalando/tip/zalenium/proxy/SauceLabsRemoteProxyTest.java
@@ -1,20 +1,32 @@
 package de.zalando.tip.zalenium.proxy;
 
+import com.google.gson.JsonObject;
+import de.zalando.tip.zalenium.util.TestUtils;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.grid.common.GridRole;
 import org.openqa.grid.common.RegistrationRequest;
 import org.openqa.grid.internal.Registry;
 import org.openqa.grid.internal.RemoteProxy;
 import org.openqa.grid.internal.TestSession;
+import org.openqa.grid.web.servlet.handler.RequestType;
+import org.openqa.grid.web.servlet.handler.WebDriverRequest;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.CapabilityType;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class SauceLabsRemoteProxyTest {
 
@@ -26,36 +38,18 @@ public class SauceLabsRemoteProxyTest {
     public static void setup() {
         registry = Registry.newInstance();
         // Creating the configuration and the registration request of the proxy (node)
-        RegistrationRequest request = new RegistrationRequest();
-        request.setRole(GridRole.NODE);
-        request.getConfiguration().put(RegistrationRequest.MAX_SESSION, 5);
-        request.getConfiguration().put(RegistrationRequest.AUTO_REGISTER, true);
-        request.getConfiguration().put(RegistrationRequest.REGISTER_CYCLE, 5000);
-        request.getConfiguration().put(RegistrationRequest.HUB_HOST, "localhost");
-        request.getConfiguration().put(RegistrationRequest.HUB_PORT, 4444);
-        request.getConfiguration().put(RegistrationRequest.PORT, 30001);
-        request.getConfiguration().put(RegistrationRequest.PROXY_CLASS, "de.zalando.tip.zalenium.proxy.SauceLabsRemoteProxy");
-        request.getConfiguration().put(RegistrationRequest.REMOTE_HOST, "http://localhost:4444");
-
+        RegistrationRequest request = TestUtils.getRegistrationRequestForTesting(30001,
+                SauceLabsRemoteProxy.class.getCanonicalName());
         sauceLabsProxy = SauceLabsRemoteProxy.getNewInstance(request, registry);
 
         // we need to register a DockerSeleniumStarter proxy to have a proper functioning SauceLabsProxy
-        request = new RegistrationRequest();
-        request.setRole(GridRole.NODE);
-        request.getConfiguration().put(RegistrationRequest.MAX_SESSION, 5);
-        request.getConfiguration().put(RegistrationRequest.AUTO_REGISTER, true);
-        request.getConfiguration().put(RegistrationRequest.REGISTER_CYCLE, 5000);
-        request.getConfiguration().put(RegistrationRequest.HUB_HOST, "localhost");
-        request.getConfiguration().put(RegistrationRequest.HUB_PORT, 4444);
-        request.getConfiguration().put(RegistrationRequest.PORT, 30002);
-        request.getConfiguration().put(RegistrationRequest.PROXY_CLASS, "de.zalando.tip.zalenium.proxy.DockerSeleniumStarterRemoteProxy");
-        request.getConfiguration().put(RegistrationRequest.REMOTE_HOST, "http://localhost:4444");
-
-        DockerSeleniumStarterRemoteProxy dockerSeleniumStarterProxy = DockerSeleniumStarterRemoteProxy.getNewInstance(request, registry);
+        request = TestUtils.getRegistrationRequestForTesting(30000,
+                DockerSeleniumStarterRemoteProxy.class.getCanonicalName());
+        DockerSeleniumStarterRemoteProxy dsStarterProxy = DockerSeleniumStarterRemoteProxy.getNewInstance(request, registry);
 
         // We add both nodes to the registry
         registry.add(sauceLabsProxy);
-        registry.add(dockerSeleniumStarterProxy);
+        registry.add(dsStarterProxy);
     }
 
     @Test
@@ -101,6 +95,47 @@ public class SauceLabsRemoteProxyTest {
         Assert.assertEquals(2, sorted.size());
         Assert.assertEquals(DockerSeleniumStarterRemoteProxy.class, sorted.get(0).getClass());
         Assert.assertEquals(SauceLabsRemoteProxy.class, sorted.get(1).getClass());
+    }
+
+    @Test
+    public void checkBeforeSessionInvocation() throws IOException {
+        // Capability which should result in a created session
+        Map<String, Object> requestedCapability = new HashMap<>();
+        requestedCapability.put(CapabilityType.BROWSER_NAME, BrowserType.SAFARI);
+        requestedCapability.put(CapabilityType.PLATFORM, Platform.MAC);
+
+        // Getting a test session in the sauce labs node
+        TestSession testSession = sauceLabsProxy.getNewSession(requestedCapability);
+        Assert.assertNotNull(testSession);
+
+        // We need to mock all the needed objects to forward the session and see how in the beforeMethod
+        // the SauceLabs user and api key get added to the body request.
+        WebDriverRequest request = mock(WebDriverRequest.class);
+        when(request.getRequestURI()).thenReturn("session");
+        when(request.getServletPath()).thenReturn("session");
+        when(request.getContextPath()).thenReturn("");
+        when(request.getMethod()).thenReturn("POST");
+        when(request.getRequestType()).thenReturn(RequestType.START_SESSION);
+        JsonObject jsonObject = new JsonObject();
+        JsonObject desiredCapabilities = new JsonObject();
+        desiredCapabilities.addProperty(CapabilityType.BROWSER_NAME, BrowserType.SAFARI);
+        desiredCapabilities.addProperty(CapabilityType.PLATFORM, Platform.MAC.name());
+        jsonObject.add("desiredCapabilities", desiredCapabilities);
+        when(request.getBody()).thenReturn(jsonObject.toString());
+
+        Enumeration<String> strings = Collections.emptyEnumeration();
+        when(request.getHeaderNames()).thenReturn(strings);
+
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        ServletOutputStream stream = mock(ServletOutputStream.class);
+        when(response.getOutputStream()).thenReturn(stream);
+
+        testSession.forward(request, response, true);
+
+        // The body should now have the SauceLabs variables
+        String expectedBody = "{\"desiredCapabilities\":{\"browserName\":\"safari\",\"platform\":\"MAC\"," +
+                "\"username\":null,\"accessKey\":null}}";
+        verify(request).setBody(expectedBody);
     }
 
 }
