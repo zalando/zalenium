953e91ebfcd1727113d91383d222d98482b769b8
Diego Fernando Molina Bocanegra <diego.fernando.molina.bocanegra@zalando.de>
2016-11-07 18:02:11 +0100

    [BUA-733] Small fix for release script, needs to be improved.

--- release.sh
+++ release.sh
@@ -2,13 +2,32 @@
 
 set -e
 
-RELEASE_VERSION=${1?"Usage $0 release-version [next-version]"}
-NEXT_VERSION=${2?"Usage $0 release-version [next-version]. Parameter 'next-version' is missing."}
+# How to use
+# ./release.sh release FIXED_VERSION, e.g. ./release.sh release 1.0.0
+# ./release.sh develop SNAPSHOT_VERSION, e.g. ./release.sh release 1.1.0-SNAPSHOT
 
-# release
-mvn scm:check-local-modification versions:set -DnewVersion=${RELEASE_VERSION} scm:add -Dincludes="**/pom.xml" scm:checkin -Dmessage="Release $RELEASE_VERSION"
+SCRIPT_ACTION=$1
+NEW_VERSION=${2?"Usage $0 release|develop version"}
 
-mvn scm:tag
+release_version()
+{
+    # release
+    mvn scm:check-local-modification versions:set -DnewVersion=${NEW_VERSION} scm:add -Dincludes="**/pom.xml" scm:checkin -Dmessage="Release $NEW_VERSION"
 
-# next development version, since it only updates the pom.xml, we skip the Travis build
-mvn versions:set -DnewVersion=${NEXT_VERSION} scm:add -Dincludes="**/pom.xml" scm:checkin -Dmessage="[skip ci]Develop $NEXT_VERSION"
+    mvn scm:tag
+}
+
+develop_version()
+{
+    # next development version, since it only updates the pom.xml, we skip the Travis build
+    mvn versions:set -DnewVersion=${NEW_VERSION} scm:add -Dincludes="**/pom.xml" scm:checkin -Dmessage="[skip ci]Develop $NEW_VERSION"
+}
+
+case ${SCRIPT_ACTION} in
+    release)
+        release_version
+    ;;
+    develop)
+        develop_version
+    ;;
+esac
