452a766815357f210b895082b91a02063db126d3
Diego Fernando Molina Bocanegra <diego.fernando.molina.bocanegra@zalando.de>
2016-11-09 17:50:42 +0100

    [BUA-723] Mapping the target folder when running tests to automatically clean it.

--- scripts/zalenium_in_docker.sh
+++ scripts/zalenium_in_docker.sh
@@ -34,7 +34,7 @@ StartUp()
 
     docker run -d -ti --name zalenium -p 4444:4444 \
           -e SAUCE_USERNAME -e SAUCE_ACCESS_KEY \
-          -v /tmp/videos:/home/seluser/videos \
+          -v ${project.build.directory}/videos:/home/seluser/videos \
           -v /var/run/docker.sock:/var/run/docker.sock \
           ${ZALENIUM_DOCKER_IMAGE} start
 
