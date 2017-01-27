6f4de3261312bd092629f847a8fd6015717da737
Diego Molina <diemol@gmail.com>
2017-01-27 19:42:53 +0100

    [BUA-864] Building image when tests are not running.

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -7,6 +7,8 @@ INTEGRATION_TO_TEST=$1
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ -n "${TRAVIS_TAG}" ] && [ "${TRAVIS_TAG}" != "latest" ]; then
     echo "TRAVIS_TAG=${TRAVIS_TAG}"
 	echo "Not running integration tests when a TAG is set, we assume they already ran in the PR."
+	echo "Building image..."
+	mvn clean package -Pbuild-docker-image -DskipTests=true
 else
     # If the environment var exists, then we run the integration tests. This is to allow external PRs ro tun
     if [ "$INTEGRATION_TO_TEST" = sauceLabs ]; then
