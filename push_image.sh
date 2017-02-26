6eb5f04bedb6a4d2f65235890f638eff27be11eb
Diego Molina <diemol@gmail.com>
2017-02-27 00:22:17 +0100

    [BUA-917] Pushing tags

--- push_image.sh
+++ push_image.sh
@@ -20,10 +20,12 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ -n "${TRAVIS_TAG}" ] && [ "${TRAVIS
     if [[ "${TRAVIS_TAG}" == "3."* ]]; then
         echo "Marking image with Selenium 3 as as zalenium:3..."
         docker tag zalenium:${TRAVIS_TAG} dosel/zalenium:3
+        docker push dosel/zalenium:3
     else
         docker tag zalenium:${TRAVIS_TAG} dosel/zalenium:latest
         docker tag zalenium:${TRAVIS_TAG} dosel/zalenium:2
         docker push dosel/zalenium:latest
+        docker push dosel/zalenium:2
     fi
 else
 	echo "Image not being pushed, either this is a PR, no tag is set, or the branch is not master."
