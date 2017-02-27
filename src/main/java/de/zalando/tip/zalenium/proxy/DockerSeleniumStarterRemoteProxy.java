5e018a3cf6d87acb6b7bde21d47d769aa7f22503
Diego Molina <diemol@gmail.com>
2017-02-27 12:14:45 +0100

    [BUA-949] Adjusting wait for video completion.

--- src/main/java/de/zalando/tip/zalenium/proxy/DockerSeleniumStarterRemoteProxy.java
+++ src/main/java/de/zalando/tip/zalenium/proxy/DockerSeleniumStarterRemoteProxy.java
@@ -345,7 +345,7 @@ public class DockerSeleniumStarterRemoteProxy extends DefaultRemoteProxy impleme
             envVariables.add("WAIT_TIMEOUT=120s");
             envVariables.add("PICK_ALL_RANDMON_PORTS=true");
             envVariables.add("PICK_ALL_RANDOM_PORTS=true");
-            envVariables.add("VIDEO_STOP_SLEEP_SECS=6");
+            envVariables.add("VIDEO_STOP_SLEEP_SECS=1");
             envVariables.add("WAIT_TIME_OUT_VIDEO_STOP=20s");
             boolean sendAnonymousUsageInfo = env.getBooleanEnvVariable("ZALENIUM_SEND_ANONYMOUS_USAGE_INFO", false);
             envVariables.add("SEND_ANONYMOUS_USAGE_INFO=" + sendAnonymousUsageInfo);
