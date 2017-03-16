bd6fa0397e9a9f87e0f746fc94c931b66526c0ff
Diego Molina <diemol@gmail.com>
2017-03-16 15:18:19 +0100

    [BUA-957] Converting FLV to MP4

--- src/main/java/de/zalando/tip/zalenium/util/CommonProxyUtilities.java
+++ src/main/java/de/zalando/tip/zalenium/util/CommonProxyUtilities.java
@@ -3,6 +3,10 @@ package de.zalando.tip.zalenium.util;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteWatchdog;
+import org.apache.commons.io.FileUtils;
 
 import java.io.*;
 import java.net.URISyntaxException;
@@ -119,6 +123,35 @@ public class CommonProxyUtilities {
                 LOG.log(Level.SEVERE, e.toString(), e);
             }
         }
+    }
+
+    public void convertFlvFileToMP4(TestInformation testInformation) {
+        // Names of the old and the new file
+        String flvVideoFile = testInformation.getVideoFolderPath() + "/" + testInformation.getFileName();
+        testInformation.setFileExtension(".mp4");
+        testInformation.buildVideoFileName();
+        String mp4VideoFile = testInformation.getVideoFolderPath() + "/" + testInformation.getFileName();
+
+        // Command to convert the file to MP4
+        CommandLine commandLine = new CommandLine("ffmpeg");
+        commandLine.addArgument("-i");
+        commandLine.addArgument(flvVideoFile);
+        commandLine.addArgument(mp4VideoFile);
+        DefaultExecutor defaultExecutor = new DefaultExecutor();
+        ExecuteWatchdog executeWatchdog = new ExecuteWatchdog(10 * 1000);
+        defaultExecutor.setWatchdog(executeWatchdog);
+        try {
+            int exitValue = defaultExecutor.execute(commandLine);
+            if (exitValue != 0) {
+                LOG.log(Level.WARNING, () -> "File " + flvVideoFile + " could not be converted to MP4. Exit value: " +
+                exitValue);
+            }
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.toString(), e);
+        }
+
+        // Deleting the FLV file
+        FileUtils.deleteQuietly(new File(flvVideoFile));
     }
 
     @SuppressWarnings("WeakerAccess")
