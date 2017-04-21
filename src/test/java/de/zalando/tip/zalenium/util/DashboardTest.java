88ba86d7e2de2e2243d358d777847e25efc3f138
Robert Zimmermann <robertzett@users.noreply.github.com>
2017-04-21 19:45:29 +0200

    Reset executed-tests-count if test-count-file is missing - refactor unit tests adapt existing TemporaryFolder approach and reuse those parts

--- src/test/java/de/zalando/tip/zalenium/util/DashboardTest.java
+++ src/test/java/de/zalando/tip/zalenium/util/DashboardTest.java
@@ -2,44 +2,33 @@ package de.zalando.tip.zalenium.util;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class DashboardTest {
-    private static final List<String> REQUIRED_FILES_FOR_TESTING = Arrays
-            .asList(("list_template.html,dashboard_template.html,zalando.ico").split(","));
-    private static final List<String> REQUIRED_DIRECTORIES_FOR_TESTING = Arrays.asList(("css,js").split(","));
 
     private TestInformation ti = new TestInformation("seleniumSessionId", "testName", "proxyName", "browser",
             "browserVersion", "platform");
 
-    @After
-    public void cleanCreatedEmptyResources() throws IOException {
-        for (String requiredFile : REQUIRED_FILES_FOR_TESTING) {
-            removeEmptyFile(requiredFile);
-        }
-        for (String requiredDir : REQUIRED_DIRECTORIES_FOR_TESTING) {
-            removeEmptyDir(requiredDir);
-        }
-    }
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
     @After
-    public void removeTestingVideosFolder() throws IOException {
-        File videos = fileWithLocalPath(Dashboard.VIDEOS_FOLDER_NAME);
-        FileUtils.deleteQuietly(videos);
+    public void restoreCommonProxyUtilities() {
+        Dashboard.restoreCommonProxyUtilities();
     }
 
     @Before
     public void initDashboard() throws IOException {
         Dashboard.setExecutedTests(0);
-        removeTestingVideosFolder();
-        ensureRequiredInputFilesExist();
+        DashboardTestingSupport.ensureRequiredInputFilesExist(temporaryFolder);
+        DashboardTestingSupport.mockCommonProxyUtilitiesForDashboardTesting(temporaryFolder);
 
         Dashboard.updateDashboard(ti);
     }
@@ -57,49 +46,13 @@ public class DashboardTest {
 
     @Test
     public void missingExecutedTestsFile() throws IOException {
-        removeTestingVideosFolder();
+        cleanTempVideosFolder();
+        DashboardTestingSupport.ensureRequiredInputFilesExist(temporaryFolder);
         Dashboard.updateDashboard(ti);
         Assert.assertEquals(1, Dashboard.getExecutedTests());
     }
 
-    private void ensureRequiredInputFilesExist() throws IOException {
-        for (String requiredFile : REQUIRED_FILES_FOR_TESTING) {
-            createEmptyFileIfMissing(requiredFile);
-        }
-        for (String requiredDir : REQUIRED_DIRECTORIES_FOR_TESTING) {
-            createEmptyDirIfMissing(requiredDir);
-        }
-    }
-
-    private void createEmptyDirIfMissing(String dirname) {
-        File toBeChecked = fileWithLocalPath(dirname);
-        if (!toBeChecked.exists()) {
-            toBeChecked.mkdir();
-        }
-    }
-
-    private File fileWithLocalPath(String name) {
-        return new File(new CommonProxyUtilities().currentLocalPath() + "/" + name);
-    }
-
-    private void createEmptyFileIfMissing(String filename) throws IOException {
-        File toBeChecked = fileWithLocalPath(filename);
-        if (!toBeChecked.exists()) {
-            toBeChecked.createNewFile();
-        }
-    }
-
-    private void removeEmptyDir(String dirname) {
-        File toBeChecked = fileWithLocalPath(dirname);
-        if (toBeChecked.exists() && toBeChecked.isDirectory() && toBeChecked.listFiles().length == 0) {
-            toBeChecked.delete();
-        }
-    }
-
-    private void removeEmptyFile(String filename) {
-        File toBeChecked = fileWithLocalPath(filename);
-        if (toBeChecked.exists() && toBeChecked.length() == 0) {
-            toBeChecked.delete();
-        }
+    private void cleanTempVideosFolder() throws IOException {
+        FileUtils.cleanDirectory(new File(temporaryFolder.getRoot().getAbsolutePath()));
     }
 }
