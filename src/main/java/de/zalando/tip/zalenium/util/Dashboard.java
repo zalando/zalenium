0665cc1585e3a40df2053dec65392fa86667e07b
Robert Zimmermann <robertzett@users.noreply.github.com>
2017-04-21 10:47:12 +0200

    Reset executed-tests-count if test-count-file is missing

--- src/main/java/de/zalando/tip/zalenium/util/Dashboard.java
+++ src/main/java/de/zalando/tip/zalenium/util/Dashboard.java
@@ -30,6 +30,11 @@ public class Dashboard {
         return executedTests;
     }
 
+    @VisibleForTesting
+    static void setExecutedTests(int executedTests) {
+        Dashboard.executedTests = executedTests;
+    }
+
     public static synchronized void updateDashboard(TestInformation testInformation) throws IOException {
         String currentLocalPath = commonProxyUtilities.currentLocalPath();
         String localVideosPath = currentLocalPath + "/" + VIDEOS_FOLDER_NAME;
@@ -60,19 +65,8 @@ public class Dashboard {
         executedTests++;
 
         File testCountFile = new File(localVideosPath, "amount_of_run_tests.txt");
-        if (testCountFile.exists()) {
-            if (isFileOlderThanOneDay(testCountFile.lastModified())) {
-                LOGGER.log(Level.FINE, "Deleting file older than one day: " + testCountFile.getAbsolutePath());
-                testCountFile.delete();
-            } else {
-                String executedTestsFromFile = FileUtils.readFileToString(testCountFile, UTF_8);
-                try {
-                    executedTests = executedTests == 1 ? Integer.parseInt(executedTestsFromFile) + 1 : executedTests;
-                } catch (Exception e) {
-                    LOGGER.log(Level.FINE, e.toString(), e);
-                }
-            }
-        }
+        synchronizeTestsCountWithFile(testCountFile);
+
         LOGGER.log(Level.FINE, "Test count: " + executedTests);
         FileUtils.writeStringToFile(testCountFile, String.valueOf(executedTests), UTF_8);
 
@@ -95,6 +89,26 @@ public class Dashboard {
         }
         if (!jsFolder.exists()) {
             FileUtils.copyDirectory(new File(currentLocalPath + "/js"), jsFolder);
+        }
+    }
+
+    @VisibleForTesting
+    static void synchronizeTestsCountWithFile(File testCountFile) throws IOException {
+        if (testCountFile.exists()) {
+            if (isFileOlderThanOneDay(testCountFile.lastModified())) {
+                LOGGER.log(Level.FINE, "Deleting file older than one day: " + testCountFile.getAbsolutePath());
+                testCountFile.delete();
+            } else {
+                String executedTestsFromFile = FileUtils.readFileToString(testCountFile, UTF_8);
+                try {
+                    executedTests = executedTests == 1 ? Integer.parseInt(executedTestsFromFile) + 1 : executedTests;
+                } catch (Exception e) {
+                    LOGGER.log(Level.FINE, e.toString(), e);
+                }
+            }
+        } else {
+            // reset executedTests if testCountFile is missing
+            executedTests = 1;
         }
     }
 
