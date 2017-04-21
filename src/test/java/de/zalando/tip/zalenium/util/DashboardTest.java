3ee53f417ce4c6b53ae98c9d9ea88be89f117461
Robert Zimmermann <robertzett@users.noreply.github.com>
2017-04-21 11:59:39 +0200

    Reset executed-tests-count if test-count-file is missing - fix testing with maven

--- src/test/java/de/zalando/tip/zalenium/util/DashboardTest.java
+++ src/test/java/de/zalando/tip/zalenium/util/DashboardTest.java
@@ -12,15 +12,41 @@ import org.junit.Test;
 public class DashboardTest {
 
     @Before
-    public void initDashboard() {
+    public void initDashboard() throws IOException {
         Dashboard.setExecutedTests(0);
         removeTestingVideosFolder();
+        ensureRequiredInputFilesExist();
+    }
+
+    private void ensureRequiredInputFilesExist() throws IOException {
+        createEmptyFileIfMissing("list_template.html");
+        createEmptyFileIfMissing("dashboard_template.html");
+        createEmptyFileIfMissing("zalando.ico");
+        createEmptyDirIfMissing("css");
+        createEmptyDirIfMissing("js");
+    }
+
+    private void createEmptyDirIfMissing(String dirname) {
+        File toBeChecked = fileWithLocalPath(dirname);
+        if (!toBeChecked.exists()) {
+            toBeChecked.mkdir();
+        }
+    }
+
+    private File fileWithLocalPath(String name) {
+        return new File(new CommonProxyUtilities().currentLocalPath() + "/" + name);
+    }
+
+    private void createEmptyFileIfMissing(String filename) throws IOException {
+        File toBeChecked = fileWithLocalPath(filename);
+        if (!toBeChecked.exists()) {
+            toBeChecked.createNewFile();
+        }
     }
 
     @After
     public void removeTestingVideosFolder() {
-        String videosPath = new CommonProxyUtilities().currentLocalPath() + "/" + Dashboard.VIDEOS_FOLDER_NAME;
-        File videos = new File(videosPath);
+        File videos = fileWithLocalPath(Dashboard.VIDEOS_FOLDER_NAME);
         FileUtils.deleteQuietly(videos);
     }
 
