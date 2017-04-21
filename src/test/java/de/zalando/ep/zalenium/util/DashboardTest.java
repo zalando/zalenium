32d907599f2bb39b9b25d2a533c1f2b5bff07b08
Robert Zimmermann <robertzett@users.noreply.github.com>
2017-04-21 15:45:00 +0200

    Reset executed-tests-count if test-count-file is missing - refactor unit tests add removal of generated empty files # Conflicts: #	src/test/java/de/zalando/ep/zalenium/util/DashboardTest.java

--- src/test/java/de/zalando/ep/zalenium/util/DashboardTest.java
+++ src/test/java/de/zalando/ep/zalenium/util/DashboardTest.java
@@ -2,6 +2,8 @@ package de.zalando.ep.zalenium.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
@@ -10,6 +12,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class DashboardTest {
+    private static final List<String> REQUIRED_FILES_FOR_TESTING = Arrays
+            .asList(("list_template.html,dashboard_template.html,zalando.ico").split(","));
+    private static final List<String> REQUIRED_DIRECTORIES_FOR_TESTING = Arrays.asList(("css,js").split(","));
 
     @Before
     public void initDashboard() throws IOException {
@@ -18,36 +23,19 @@ public class DashboardTest {
         ensureRequiredInputFilesExist();
     }
 
-    private void ensureRequiredInputFilesExist() throws IOException {
-        createEmptyFileIfMissing("list_template.html");
-        createEmptyFileIfMissing("dashboard_template.html");
-        createEmptyFileIfMissing("zalando.ico");
-        createEmptyDirIfMissing("css");
-        createEmptyDirIfMissing("js");
-    }
-
-    private void createEmptyDirIfMissing(String dirname) {
-        File toBeChecked = fileWithLocalPath(dirname);
-        if (!toBeChecked.exists()) {
-            toBeChecked.mkdir();
+    @After
+    public void cleanCreatedEmptyResources() throws IOException {
+        for (String requiredFile : REQUIRED_FILES_FOR_TESTING) {
+            removeEmptyFile(requiredFile);
         }
-    }
-
-    private File fileWithLocalPath(String name) {
-        return new File(new CommonProxyUtilities().currentLocalPath() + "/" + name);
-    }
-
-    private void createEmptyFileIfMissing(String filename) throws IOException {
-        File toBeChecked = fileWithLocalPath(filename);
-        if (!toBeChecked.exists()) {
-            toBeChecked.createNewFile();
+        for (String requiredDir : REQUIRED_DIRECTORIES_FOR_TESTING) {
+            removeEmptyDir(requiredDir);
         }
     }
 
     @After
-    public void removeTestingVideosFolder() {
-        String videosPath = new CommonProxyUtilities().currentLocalPath() + "/" + Dashboard.VIDEOS_FOLDER_NAME;
-        File videos = new File(videosPath);
+    public void removeTestingVideosFolder() throws IOException {
+        File videos = fileWithLocalPath(Dashboard.VIDEOS_FOLDER_NAME);
         FileUtils.deleteQuietly(videos);
     }
 
@@ -87,5 +75,46 @@ public class DashboardTest {
     private TestInformation createDefaultTestInformation() {
         return new TestInformation("seleniumSessionId", "testName", "proxyName", "browser", "browserVersion",
                 "platform");
+    }
+
+    private void ensureRequiredInputFilesExist() throws IOException {
+        for (String requiredFile : REQUIRED_FILES_FOR_TESTING) {
+            createEmptyFileIfMissing(requiredFile);
+        }
+        for (String requiredDir : REQUIRED_DIRECTORIES_FOR_TESTING) {
+            createEmptyDirIfMissing(requiredDir);
+        }
+    }
+
+    private void createEmptyDirIfMissing(String dirname) {
+        File toBeChecked = fileWithLocalPath(dirname);
+        if (!toBeChecked.exists()) {
+            toBeChecked.mkdir();
+        }
+    }
+
+    private File fileWithLocalPath(String name) {
+        return new File(new CommonProxyUtilities().currentLocalPath() + "/" + name);
+    }
+
+    private void createEmptyFileIfMissing(String filename) throws IOException {
+        File toBeChecked = fileWithLocalPath(filename);
+        if (!toBeChecked.exists()) {
+            toBeChecked.createNewFile();
+        }
+    }
+
+    private void removeEmptyDir(String dirname) {
+        File toBeChecked = fileWithLocalPath(dirname);
+        if (toBeChecked.exists() && toBeChecked.isDirectory() && toBeChecked.listFiles().length == 0) {
+            toBeChecked.delete();
+        }
+    }
+
+    private void removeEmptyFile(String filename) {
+        File toBeChecked = fileWithLocalPath(filename);
+        if (toBeChecked.exists() && toBeChecked.length() == 0) {
+            toBeChecked.delete();
+        }
     }
 }
