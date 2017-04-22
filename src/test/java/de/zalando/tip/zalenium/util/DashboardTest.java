cfd3057d60eb66aefb9d9ffce084e57fc8e12788
Robert Zimmermann <robertzett@users.noreply.github.com>
2017-04-22 10:19:20 +0200

    Reset executed-tests-count if test-count-file is missing - add test for NumberFormatException

--- src/test/java/de/zalando/tip/zalenium/util/DashboardTest.java
+++ src/test/java/de/zalando/tip/zalenium/util/DashboardTest.java
@@ -1,5 +1,7 @@
 package de.zalando.tip.zalenium.util;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -12,6 +14,8 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 public class DashboardTest {
+
+    public static final String TEST_COUNT_FILE_NAME = "amount_of_run_tests.txt";
 
     private TestInformation ti = new TestInformation("seleniumSessionId", "testName", "proxyName", "browser",
             "browserVersion", "platform");
@@ -50,6 +54,16 @@ public class DashboardTest {
         DashboardTestingSupport.ensureRequiredInputFilesExist(temporaryFolder);
         Dashboard.updateDashboard(ti);
         Assert.assertEquals(1, Dashboard.getExecutedTests());
+    }
+
+    @Test
+    public void nonNumberContentsIgnored() throws IOException {
+        File testCountFile = new File(temporaryFolder.getRoot().getAbsolutePath() + "/" + Dashboard.VIDEOS_FOLDER_NAME
+                + "/" + TEST_COUNT_FILE_NAME);
+        FileUtils.writeStringToFile(testCountFile, "Not-A-Number", UTF_8);
+        Dashboard.setExecutedTests(0);
+        Dashboard.updateDashboard(ti);
+        Assert.assertEquals("1", FileUtils.readFileToString(testCountFile, UTF_8));
     }
 
     private void cleanTempVideosFolder() throws IOException {
