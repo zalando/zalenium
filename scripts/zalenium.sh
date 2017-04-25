43d33274bcd35949db044ee136ba0892355f5df3
Leo Gallucci <elgalu3@gmail.com>
2017-04-25 16:27:36 +0200

    [BUA-1022] Improve jq json line

--- scripts/zalenium.sh
+++ scripts/zalenium.sh
@@ -137,9 +137,9 @@ WaitTestingBotProxy()
 export -f WaitTestingBotProxy
 
 WaitForVideosTransferred() {
-    local __amount_of_tests_with_video=$(</home/seluser/videos/executedTestsInfo.json | jq .executedTestsWithVideo)
+    local __amount_of_tests_with_video=$(jq .executedTestsWithVideo /home/seluser/videos/executedTestsInfo.json)
 
-    if [ "${__amount_of_tests_with_video}" -gt 0 ]; then
+    if [ ${__amount_of_tests_with_video} -gt 0 ]; then
         local __amount_of_mp4_files=$(ls -1q /home/seluser/videos/*.mp4 | wc -l)
         while [ "${__amount_of_mp4_files}" -lt "${__amount_of_tests_with_video}" ]; do
             log "Waiting for ${__amount_of_mp4_files} mp4 files to be a total of ${__amount_of_tests_with_video}..."
@@ -148,7 +148,7 @@ WaitForVideosTransferred() {
             # Also check if there are mkv, this would mean that
             # docker-selenium failed to convert them to mp4
             local __amount_of_mkv_files=$(ls -1q /home/seluser/videos/*.mkv | wc -l)
-            if [ "${__amount_of_mkv_files}" -gt 0 ]; then
+            if [ ${__amount_of_mkv_files} -gt 0 ]; then
                 for __filename in /home/seluser/videos/*.mkv; do
                     local __new_file_name="$(basename ${__filename} .mkv).mp4"
                     log "Renaming ${__filename} into ${__new_file_name} ..."
