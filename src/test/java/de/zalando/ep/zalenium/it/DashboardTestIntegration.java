42ff198c0311081056d8a263eccc71239290e7ed
Robert Zimmermann <robertzett@users.noreply.github.com>
2017-05-09 12:06:44 +0200

    Remove unused imports

--- src/test/java/de/zalando/ep/zalenium/it/DashboardTestIntegration.java
+++ src/test/java/de/zalando/ep/zalenium/it/DashboardTestIntegration.java
@@ -12,8 +12,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.safari.SafariDriver;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
