0499ce6a2bd2d55c476a4de8666035027f82f3be
Matt Fox <matt.fox@logmein.com>
2017-05-10 16:13:53 -0700

    Fixed grep for container id when running in DinD

--- scripts/zalenium.sh
+++ scripts/zalenium.sh
@@ -256,7 +256,7 @@ trap DockerTerminate SIGTERM SIGINT SIGKILL
 StartUp()
 {
     EnsureDockerWorks
-    CONTAINER_ID=$(grep docker /proc/self/cgroup | grep -o -E '[0-9a-f]{64}' | head -n 1)
+    CONTAINER_ID=$(grep docker /proc/self/cgroup | grep -o -E '[0-9a-f]{64}$' | head -n 1)
     CONTAINER_NAME=$(docker inspect ${CONTAINER_ID} | jq -r '.[0].Name' | sed 's/\///g')
     CONTAINER_LIVE_PREVIEW_PORT=$(docker inspect ${CONTAINER_ID} | jq -r '.[0].NetworkSettings.Ports."5555/tcp"' | jq -r '.[0].HostPort')
     EnsureCleanEnv
