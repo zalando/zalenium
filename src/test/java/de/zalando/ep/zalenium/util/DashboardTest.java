0e970e72dafef22cd863b12b8c3553340398ca4d
Diego Molina <diemol@gmail.com>
2017-05-17 20:05:00 +0200

    Reset executed-tests-count if test-count-file is missing - refactor unit tests adapt existing TemporaryFolder approach and reuse those parts # Conflicts: #	src/test/java/de/zalando/ep/zalenium/util/DashboardTest.java

--- src/test/java/de/zalando/ep/zalenium/util/DashboardTest.java
+++ src/test/java/de/zalando/ep/zalenium/util/DashboardTest.java
@@ -5,6 +5,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import java.io.File;
 import java.io.IOException;
 
+import com.google.gson.JsonObject;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Assert;
@@ -15,7 +16,7 @@ import org.junit.rules.TemporaryFolder;
 
 public class DashboardTest {
 
-    public static final String TEST_COUNT_FILE_NAME = "amount_of_run_tests.txt";
+    private static final String TEST_COUNT_FILE_NAME = "executedTestsInfo.json";
 
     private TestInformation ti = new TestInformation("seleniumSessionId", "testName", "proxyName", "browser",
             "browserVersion", "platform");
@@ -64,10 +65,14 @@ public class DashboardTest {
     public void nonNumberContentsIgnored() throws IOException {
         File testCountFile = new File(temporaryFolder.getRoot().getAbsolutePath() + "/" + Dashboard.VIDEOS_FOLDER_NAME
                 + "/" + TEST_COUNT_FILE_NAME);
-        FileUtils.writeStringToFile(testCountFile, "Not-A-Number", UTF_8);
-        Dashboard.setExecutedTests(0);
+        JsonObject testQuantities = new JsonObject();
+        testQuantities.addProperty("executedTests", "Not-A-Number");
+        testQuantities.addProperty("executedTestsWithVideo", "Not-A-Number");
+        FileUtils.writeStringToFile(testCountFile, testQuantities.toString(), UTF_8);
+        Dashboard.setExecutedTests(0, 0);
         Dashboard.updateDashboard(ti);
-        Assert.assertEquals("1", FileUtils.readFileToString(testCountFile, UTF_8));
+        Assert.assertEquals(1, Dashboard.getExecutedTests());
+        Assert.assertEquals(1, Dashboard.getExecutedTestsWithVideo());
     }
 
     private void cleanTempVideosFolder() throws IOException {
