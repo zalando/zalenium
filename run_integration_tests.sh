56e99bcd3654d556a1017af9ef7b30b552f46163
Diego Molina <diemol@gmail.com>
2017-06-26 14:28:36 +0200

    Generating artefacts needed for docker-compose test

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -42,6 +42,7 @@ else
     if [ "$INTEGRATION_TO_TEST" = dockerCompose ]; then
         if [ -n "${SAUCE_USERNAME}" ]; then
             sudo mvn clean
+            mvn clean package -DskipTests=true
             mkdir -p "${VIDEOS_FOLDER}"
             chmod +x target/zalenium_in_docker_compose.sh
             target/zalenium_in_docker_compose.sh start
