ad8605e81c34a9850f91398c3c4e1f9d51c66a6d
Diego Molina <diemol@gmail.com>
2017-09-17 16:37:40 +0200

    [BUA-1110] Reworking core logic for matching capabilities and processing requests since the requestedCapabilities object is now an immutableMap, and it cannot be changed...

---
+++ src/main/java/de/zalando/ep/zalenium/util/ProcessedCapabilities.java
@@ -0,0 +1,46 @@
+package de.zalando.ep.zalenium.util;
+
+import java.util.Map;
+
+public class ProcessedCapabilities {
+    private Map<String, Object> requestedCapability;
+    private int identityHashCode;
+    private long lastProcessedTime;
+    private int processedTimes;
+
+    public ProcessedCapabilities(Map<String, Object> requestedCapability, int identityHashCode) {
+        this.requestedCapability = requestedCapability;
+        this.identityHashCode = identityHashCode;
+        this.lastProcessedTime = System.currentTimeMillis();
+        this.processedTimes = 1;
+    }
+
+    public Map<String, Object> getRequestedCapability() {
+        return requestedCapability;
+    }
+
+    public void setRequestedCapability(Map<String, Object> requestedCapability) {
+        this.requestedCapability = requestedCapability;
+    }
+
+    public int getIdentityHashCode() {
+        return identityHashCode;
+    }
+
+    public long getLastProcessedTime() {
+        return lastProcessedTime;
+    }
+
+    public void setLastProcessedTime(long lastProcessedTime) {
+        this.lastProcessedTime = lastProcessedTime;
+    }
+
+    public int getProcessedTimes() {
+        return processedTimes;
+    }
+
+    public void setProcessedTimes(int processedTimes) {
+        this.processedTimes = processedTimes;
+    }
+
+}
