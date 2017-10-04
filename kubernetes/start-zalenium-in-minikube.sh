3a7bfcfe0c740f6624bb8dcaed8c3b466644384b
Diego Molina <diemol@gmail.com>
2017-10-04 12:44:27 +0200

    [BUA-1113] Using correct file names

--- kubernetes/start-zalenium-in-minikube.sh
+++ kubernetes/start-zalenium-in-minikube.sh
@@ -11,8 +11,8 @@
 ./kubectl create --validate=false -f kubernetes/service.yaml
 ./kubectl get services
 
-MINIKUBE_IP=$(minikube ip)
-ZALENIUM_GRID_PORT=$(kubectl get svc zalenium -o go-template='{{ index (index .spec.ports 0) "nodePort" }}{{ "\n" }}')
+MINIKUBE_IP=$(./minikube ip)
+ZALENIUM_GRID_PORT=$(./kubectl get svc zalenium -o go-template='{{ index (index .spec.ports 0) "nodePort" }}{{ "\n" }}')
 
 echo $MINIKUBE_IP:$ZALENIUM_GRID_PORT/wd/hub/status
 
