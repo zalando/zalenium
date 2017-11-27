33ae940a1b8cbf9ba02a820d69ab5cb805078327
Diego Molina <diemol@users.noreply.github.com>
2017-11-27 20:03:25 +0100

    Handling requests with browser version in starter proxy (#337)
    * Handling the absence of browser version in the starter proxy. Always copying capabilities to keep them immutable.

--- src/main/java/de/zalando/ep/zalenium/matcher/DockerSeleniumCapabilityMatcher.java
+++ src/main/java/de/zalando/ep/zalenium/matcher/DockerSeleniumCapabilityMatcher.java
@@ -2,14 +2,22 @@ package de.zalando.ep.zalenium.matcher;
 
 import de.zalando.ep.zalenium.proxy.DockerSeleniumRemoteProxy;
 import de.zalando.ep.zalenium.proxy.DockerSeleniumStarterRemoteProxy;
+import org.openqa.grid.internal.TestSlot;
 import org.openqa.grid.internal.utils.DefaultCapabilityMatcher;
 import org.openqa.grid.selenium.proxy.DefaultRemoteProxy;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.CapabilityType;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DockerSeleniumCapabilityMatcher extends DefaultCapabilityMatcher {
+    private static String chromeVersion = null;
+    private static String firefoxVersion = null;
+    private static AtomicBoolean browserVersionsFetched = new AtomicBoolean(false);
     private final Logger logger = Logger.getLogger(DockerSeleniumCapabilityMatcher.class.getName());
     private DefaultRemoteProxy proxy;
 
@@ -23,16 +31,55 @@ public class DockerSeleniumCapabilityMatcher extends DefaultCapabilityMatcher {
         logger.log(Level.FINE, ()-> String.format("Validating %s in node with capabilities %s", requestedCapability,
                 nodeCapability));
 
+        Map<String, Object> requestedCapabilityCopy = copyMap(requestedCapability);
+        Map<String, Object> nodeCapabilityCopy = copyMap(nodeCapability);
+
         boolean screenResolutionMatches = true;
         boolean timeZoneCapabilityMatches = true;
         // This validation is only done for docker-selenium nodes
         if (proxy instanceof DockerSeleniumRemoteProxy) {
-            screenResolutionMatches = isScreenResolutionMatching(nodeCapability, requestedCapability);
-            timeZoneCapabilityMatches = isTimeZoneMatching(nodeCapability, requestedCapability);
+            getChromeAndFirefoxVersions(proxy);
+            screenResolutionMatches = isScreenResolutionMatching(nodeCapabilityCopy, requestedCapabilityCopy);
+            timeZoneCapabilityMatches = isTimeZoneMatching(nodeCapabilityCopy, requestedCapabilityCopy);
+        } else if (proxy instanceof DockerSeleniumStarterRemoteProxy) {
+            // We do this because the starter node does not have the browser versions
+            if (browserVersionsFetched.get()) {
+                String browser = nodeCapability.get(CapabilityType.BROWSER_NAME).toString();
+                if (BrowserType.FIREFOX.equalsIgnoreCase(browser)) {
+                    nodeCapabilityCopy.put(CapabilityType.VERSION, firefoxVersion);
+                } else if (BrowserType.CHROME.equalsIgnoreCase(browser)) {
+                    nodeCapabilityCopy.put(CapabilityType.VERSION, chromeVersion);
+                }
+            } else {
+                requestedCapabilityCopy.remove(CapabilityType.VERSION);
+            }
         }
 
-        return super.matches(nodeCapability, requestedCapability) && screenResolutionMatches &&
+        return super.matches(nodeCapabilityCopy, requestedCapabilityCopy) && screenResolutionMatches &&
                 timeZoneCapabilityMatches;
+    }
+
+    // Cannot use Collectors.toMap() because it fails when there are null values.
+    private Map<String, Object> copyMap(Map<String, Object> mapToCopy) {
+        Map<String, Object> copiedMap = new HashMap<>();
+        for (Map.Entry<String, Object> entry : mapToCopy.entrySet()) {
+            copiedMap.put(entry.getKey(), entry.getValue());
+        }
+        return copiedMap;
+    }
+
+    private void getChromeAndFirefoxVersions(DefaultRemoteProxy proxy) {
+        if (!browserVersionsFetched.getAndSet(true)) {
+            for (TestSlot testSlot : proxy.getTestSlots()) {
+                String browser = testSlot.getCapabilities().get(CapabilityType.BROWSER_NAME).toString();
+                String browserVersion = testSlot.getCapabilities().get(CapabilityType.VERSION).toString();
+                if (BrowserType.CHROME.equalsIgnoreCase(browser)) {
+                    chromeVersion = browserVersion;
+                } else if (BrowserType.FIREFOX.equalsIgnoreCase(browser)) {
+                    firefoxVersion = browserVersion;
+                }
+            }
+        }
     }
 
     private boolean isScreenResolutionMatching(Map<String, Object> nodeCapability, Map<String, Object> requestedCapability) {
