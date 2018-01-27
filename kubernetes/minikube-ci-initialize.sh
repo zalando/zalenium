36af7bae6638685864f87525e135498d5724df96
Diego Molina <diemol@gmail.com>
2018-01-27 18:59:04 +0100

    Fixing path

--- kubernetes/minikube-ci-initialize.sh
+++ kubernetes/minikube-ci-initialize.sh
@@ -31,7 +31,7 @@ mkdir $HOME/.kube &> /dev/null || true
 touch $HOME/.kube/config
 
 export KUBECONFIG=$HOME/.kube/config
-sudo -E ./minikube start --vm-driver=none --extra-config=apiserver.InsecureServingOptions.BindAddress="127.0.0.1" --extra-config=apiserver.InsecureServingOptions.BindPort="8080"
+sudo -E minikube start --vm-driver=none --extra-config=apiserver.InsecureServingOptions.BindAddress="127.0.0.1" --extra-config=apiserver.InsecureServingOptions.BindPort="8080"
 
 # this for loop waits until kubectl can access the api server that minikube has created
 KUBECTL_UP="false"
