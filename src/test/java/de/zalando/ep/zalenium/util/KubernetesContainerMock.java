bafbc3836775560e4a2be1772712ce609ea9017a
Diego Molina <diemol@gmail.com>
2018-03-04 07:12:28 -0800

    Handling possible null

--- src/test/java/de/zalando/ep/zalenium/util/KubernetesContainerMock.java
+++ src/test/java/de/zalando/ep/zalenium/util/KubernetesContainerMock.java
@@ -52,6 +52,9 @@ public class KubernetesContainerMock {
         } catch (UnknownHostException e) {
             hostName = "";
         }
+        if (hostName == null) {
+            hostName = "localhost";
+        }
         KubernetesServer server = new KubernetesServer();
         server.before();
 
