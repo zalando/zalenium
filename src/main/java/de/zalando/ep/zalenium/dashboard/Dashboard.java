b1b7188e30c820513f60ea69ee76ddf3ca1041c0
Diego Molina <diemol@users.noreply.github.com>
2019-05-04 07:21:51 +0200

    Fixing dashboard to display numeric build numbers. (#963)

--- src/main/java/de/zalando/ep/zalenium/dashboard/Dashboard.java
+++ src/main/java/de/zalando/ep/zalenium/dashboard/Dashboard.java
@@ -21,7 +21,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.io.IOUtils;
