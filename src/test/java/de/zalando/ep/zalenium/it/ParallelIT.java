5adcdce816e8e54a44581df6feea6b7a74eeea92
Diego Molina <diemol@gmail.com>
2020-03-23 19:26:00 +0100

    Removing unused imports.

--- src/test/java/de/zalando/ep/zalenium/it/ParallelIT.java
+++ src/test/java/de/zalando/ep/zalenium/it/ParallelIT.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.net.NetworkUtils;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
