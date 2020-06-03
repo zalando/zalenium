11a3c13e26a275608a66fae4bddc734605db6b93
Stefan Birkner <mail@stefan-birkner.de>
2020-06-03 21:15:23 +0200

    Use System Lambda instead of System Rules
    System Lambda is independent of the test framework. This makes it
    possible to move to another test framework (e.g. JUnit Jupiter) in the
    future.

--- src/test/java/de/zalando/ep/zalenium/util/EnvironmentTest.java
+++ src/test/java/de/zalando/ep/zalenium/util/EnvironmentTest.java
@@ -1,12 +1,9 @@
 package de.zalando.ep.zalenium.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import io.fabric8.kubernetes.api.model.*;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.contrib.java.lang.system.EnvironmentVariables;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,27 +11,26 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static com.github.stefanbirkner.systemlambda.SystemLambda.withEnvironmentVariable;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 
 public class EnvironmentTest {
 
-    @Rule
-    public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
-
     @Test
-    public void getDoubleArrayEnvVariable() {
+    public void getDoubleArrayEnvVariable() throws Exception {
         String doubleArrayFormat = "0.5,2.5,5,10,15,20,25,30,35,40,50,60";
         String testSessionLatencyBuckets = "ZALENIUM_TEST_SESSION_LATENCY_BUCKETS";
-        environmentVariables.set(testSessionLatencyBuckets, doubleArrayFormat);
-
-        double[] doubleArrayEnvVariable = new Environment().getDoubleArrayEnvVariable(testSessionLatencyBuckets);
-        assertArrayEquals(doubleArrayEnvVariable, new double[]{0.5,2.5,5,10,15,20,25,30,35,40,50,60}, 0);
+        withEnvironmentVariable(testSessionLatencyBuckets, doubleArrayFormat)
+            .execute(() -> {
+                double[] doubleArrayEnvVariable = new Environment().getDoubleArrayEnvVariable(testSessionLatencyBuckets);
+                assertArrayEquals(doubleArrayEnvVariable, new double[]{0.5,2.5,5,10,15,20,25,30,35,40,50,60}, 0);
+            });
     }
 
     @Test
-    public void getYamlListEnvVariable() throws JsonProcessingException {
+    public void getYamlListEnvVariable() throws Exception {
         List<Toleration> givenTolerations = new ArrayList<>();
 
         givenTolerations.add(
@@ -57,33 +53,35 @@ public class EnvironmentTest {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
 
         String kubernetesTolerationsEnvVar = "ZALENIUM_KUBERNETES_TOLERATIONS";
-        environmentVariables.set(kubernetesTolerationsEnvVar, mapper.writeValueAsString(givenTolerations));
+        withEnvironmentVariable(kubernetesTolerationsEnvVar, mapper.writeValueAsString(givenTolerations))
+            .execute(() -> {
+                // example of yaml string for env: "---\n- effect: \"NoExecute\"\n  key: \"no-cluster-autoscaling\"\n  operator: \"Equal\"\n  value: \"false\"\n- effect: \"NoExecute\"\n  key: \"cluster-autoscaling\"\n  operator: \"Equal\"\n  value: \"true\""
+                // OR
+                //---
+                //- effect: "NoExecute"
+                //  key: "no-cluster-autoscaling"
+                //  operator: "Equal"
+                //  value: "false"
+                //- effect: "NoExecute"
+                //  key: "cluster-autoscaling"
+                //  operator: "Equal"
+                //  value: "true"
 
-        // example of yaml string for env: "---\n- effect: \"NoExecute\"\n  key: \"no-cluster-autoscaling\"\n  operator: \"Equal\"\n  value: \"false\"\n- effect: \"NoExecute\"\n  key: \"cluster-autoscaling\"\n  operator: \"Equal\"\n  value: \"true\""
-        // OR
-        //---
-        //- effect: "NoExecute"
-        //  key: "no-cluster-autoscaling"
-        //  operator: "Equal"
-        //  value: "false"
-        //- effect: "NoExecute"
-        //  key: "cluster-autoscaling"
-        //  operator: "Equal"
-        //  value: "true"
+                List<Toleration> tolerationsList = new Environment().getYamlListEnvVariable(kubernetesTolerationsEnvVar, Toleration.class, new ArrayList<Toleration>());
+                int i=0;
+                for (Toleration toleration : tolerationsList) {
+                    assertEquals(toleration.getKey(), givenTolerations.get(i).getKey());
+                    assertEquals(toleration.getOperator(), givenTolerations.get(i).getOperator());
+                    assertEquals(toleration.getEffect(), givenTolerations.get(i).getEffect());
+                    assertEquals(toleration.getValue(), givenTolerations.get(i).getValue());
+                    i++;
+                }
 
-        List<Toleration> tolerationsList = new Environment().getYamlListEnvVariable(kubernetesTolerationsEnvVar, Toleration.class, new ArrayList<Toleration>());
-        int i=0;
-        for (Toleration toleration : tolerationsList) {
-            assertEquals(toleration.getKey(), givenTolerations.get(i).getKey());
-            assertEquals(toleration.getOperator(), givenTolerations.get(i).getOperator());
-            assertEquals(toleration.getEffect(), givenTolerations.get(i).getEffect());
-            assertEquals(toleration.getValue(), givenTolerations.get(i).getValue());
-            i++;
-        }
+            });
     }
 
     @Test
-    public void getMapEnvVariable() {
+    public void getMapEnvVariable() throws Exception {
         Map<String,String> nodeSelector = new HashMap<>();
 
         nodeSelector.put("beta.kubernetes.io/os", "linux");
@@ -95,10 +93,11 @@ public class EnvironmentTest {
                 .collect(Collectors.joining(","));
 
         String kubernetesNodeSelectorEnvVar = "ZALENIUM_KUBERNETES_NODE_SELECTOR";
-        environmentVariables.set(kubernetesNodeSelectorEnvVar, nodeSelectorAsString);
-
-        Map<String, String> nodeSelectorMap = new Environment().getMapEnvVariable(kubernetesNodeSelectorEnvVar, new HashMap<>());
-        assertEquals(nodeSelectorMap, nodeSelector);
+        withEnvironmentVariable(kubernetesNodeSelectorEnvVar, nodeSelectorAsString)
+            .execute(() -> {
+                Map<String, String> nodeSelectorMap = new Environment().getMapEnvVariable(kubernetesNodeSelectorEnvVar, new HashMap<>());
+                assertEquals(nodeSelectorMap, nodeSelector);
+            });
     }
 
 }
